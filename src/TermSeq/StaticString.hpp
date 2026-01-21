4dc8ee4e08815bfc0b4e4e69bc769334b96f562f
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2026-01-20 23:37:51 -0600

    Add documentation

--- src/TermSeq/StaticString.hpp
+++ src/TermSeq/StaticString.hpp
@@ -8,9 +8,15 @@
 
 namespace termseq {
 
+/**
+ * Compile-time string
+ */
 template <std::size_t>
 struct StaticString;
 
+/**
+ * Join character array-like objects into a single `termseq::StaticString`.
+ */
 template <std::ranges::sized_range... Args>
   requires(std::same_as<char, std::ranges::range_value_t<Args>> && ...)
 consteval auto joinStaticStrings(Args&&... args)
@@ -19,47 +25,65 @@ consteval auto joinStaticStrings(Args&&... args)
 }  // namespace termseq
 
 template <std::size_t N>
-struct std::formatter<termseq::StaticString<N>, char>
-    : public std::formatter<std::string_view, char> {
-  using std::formatter<std::string_view, char>::parse;
-
-  template <typename FormatContext>
-  FormatContext::iterator format(const termseq::StaticString<N>& s,
-                                 FormatContext& ctx) const {
-    return std::formatter<std::string_view, char>::format(s, ctx);
-  }
-};
-
-template <std::size_t N>
 struct termseq::StaticString {
+  /**
+   * Constructs default empty character array
+   */
   constexpr StaticString() {}
 
   constexpr StaticString(const StaticString&) = default;
 
+  /**
+   * Copys the c-string verbatim.
+   */
   constexpr StaticString(const char (&cstr)[N]) {
     std::ranges::copy_n(cstr, N, m_data);
   }
 
+  /**
+   * Identical to `StaticString(const char(&)[N])`.
+   *
+   * Facilitates easier concatenation.
+   */
   constexpr StaticString(const std::array<char, N>& cstr) {
     std::ranges::copy(cstr, m_data);
   }
 
+  /**
+   * Converts into a view of the string contents.
+   */
   constexpr operator std::string_view() const {
     return std::string_view{data(), size()};
   }
 
+  /**
+   * First iterator of the characters.
+   */
   constexpr auto begin(this auto&& self) { return self.data(); }
 
+  /**
+   * Iterator just past the last position.
+   */
   constexpr auto end(this auto&& self) { return self.data() + self.size(); }
 
+  /**
+   * Returns the underlying pointer.
+   */
   constexpr auto data(this auto&& self) { return self.m_data; }
 
+  /**
+   * Size of the character array as it is stored.
+   */
   static constexpr std::size_t size() { return N; }
 
  private:
   char m_data[N]{};
 };
 
+/**
+ * Compares `termseq::StaticString` by first converting to `std::string_view`,
+ * then using its implementation.
+ */
 template <std::size_t L, std::size_t R>
 constexpr bool operator==(const termseq::StaticString<L>& lhs,
                           const termseq::StaticString<R>& rhs) {
@@ -67,6 +91,10 @@ constexpr bool operator==(const termseq::StaticString<L>& lhs,
          static_cast<std::string_view>(rhs);
 }
 
+/**
+ * Concatenates given character sequences into a singular
+ * `termseq::StaticString`.
+ */
 template <std::ranges::sized_range... Args>
   requires(std::same_as<char, std::ranges::range_value_t<Args>> && ...)
 consteval auto termseq::joinStaticStrings(Args&&... args)
@@ -80,5 +108,21 @@ consteval auto termseq::joinStaticStrings(Args&&... args)
    ...);
   return StaticString{data};
 }
+
+/**
+ * Formatter which borrows its implementation from the `std::string_view`
+ * formatter.
+ */
+template <std::size_t N>
+struct std::formatter<termseq::StaticString<N>, char>
+    : public std::formatter<std::string_view, char> {
+  using std::formatter<std::string_view, char>::parse;
+
+  template <typename FormatContext>
+  FormatContext::iterator format(const termseq::StaticString<N>& s,
+                                 FormatContext& ctx) const {
+    return std::formatter<std::string_view, char>::format(s, ctx);
+  }
+};
 
 #endif

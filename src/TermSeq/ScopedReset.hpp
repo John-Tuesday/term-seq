814752e6d463fcecece748d833e5348d568a4805
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2026-01-29 00:45:35 -0600

    Add generalized scoped output class
    Base implementation of an object which writes something on destruction.
    Use this base class to implement format resetting and cursor restoring
    automatically within a scope.

--- src/TermSeq/ScopedReset.hpp
+++ src/TermSeq/ScopedReset.hpp
@@ -5,38 +5,120 @@
 
 #include <TermSeq>
 
+#include <cassert>
+#include <utility>
+
+/**
+ * Type constraints.
+ */
+namespace termseq::concepts {
+
+/**
+ * An object of given type `R` can be inserted into another object of type `T`
+ * using the operator `<<`.
+ */
+template <typename T, typename R>
+concept InsertableInto = requires(T t, R r) { t << r; };
+
+/**
+ * Requires `T` to be a type which can be printed to and `R` to be formattable
+ * using `CharT`.
+ */
+template <typename T, typename CharT, typename R>
+concept PrintableWith = std::formattable<R, CharT> &&
+                        requires(T t, R r) { std::print(t, "{}", r); };
+
+}  // namespace termseq::concepts
+
 namespace termseq {
+
+/**
+ * Write to given sequence to output on destruction.
+ *
+ * RAII style.
+ */
+template <typename T, typename S, typename C = char>
+  requires termseq::concepts::PrintableWith<T, C, S> ||
+           termseq::concepts::InsertableInto<T, S>
+class ScopedOutput {
+ public:
+  using CharT = C;
+  using Seq = S;
+  using Stream = T;
+
+  template <typename R, typename U>
+  ScopedOutput(R&& out, U&& seq)
+      : m_valid{true},
+        m_out{std::forward<R>(out)},
+        m_seq{std::forward<U>(seq)} {}
+
+  ~ScopedOutput() {
+    if (valid())
+      release();
+  }
+
+  template <typename Self>
+  auto out(this Self&& self) -> decltype(auto) {
+    return std::forward_like<Self>(self.m_out);
+  }
+
+  void release(this auto&& self) {
+    assert(self.valid());
+    if constexpr (termseq::concepts::PrintableWith<Stream, CharT, Seq>) {
+      std::print(self.out(), "{}", self.m_seq);
+    } else if constexpr (termseq::concepts::InsertableInto<Stream, Seq>) {
+      self.out() << self.m_seq;
+    } else {
+      std::unreachable();
+    }
+    self.m_valid = false;
+  }
+
+  bool valid() const { return m_valid; }
+
+ private:
+  bool m_valid{false};
+  Stream m_out;
+  Seq m_seq;
+};
+
+template <typename T, typename U>
+ScopedOutput(T&, U&) -> ScopedOutput<T&, U&>;
+template <typename T, typename U>
+ScopedOutput(T&&, U&&) -> ScopedOutput<T, U>;
+
+/**
+ * Restore cursor on destruction.
+ *
+ * RAII style.
+ */
+template <typename T>
+class ScopedRestoreCursor
+    : public termseq::ScopedOutput<T, decltype(termseq::restoreCursor)> {
+  using Parent = termseq::ScopedOutput<T, decltype(termseq::restoreCursor)>;
+
+ public:
+  template <typename S>
+  ScopedRestoreCursor(S&& out)
+      : Parent{std::forward<S>(out), termseq::restoreCursor} {}
+};
+
+template <typename T>
+ScopedRestoreCursor(T&) -> ScopedRestoreCursor<T&>;
+template <typename T>
+ScopedRestoreCursor(T&&) -> ScopedRestoreCursor<T>;
 
 /**
  * Reset formatting on destruction RAII.
  */
 template <typename T>
-  requires requires(T t) {
-    { t << termseq::reset };
-  }
-class ScopedReset {
+class ScopedReset : public termseq::ScopedOutput<T, decltype(termseq::reset)> {
+  using ScopedOutputParent = termseq::ScopedOutput<T, decltype(termseq::reset)>;
+
  public:
-  using Stream = T;
-
   template <typename S>
-  ScopedReset(S&& out) : m_valid{true}, m_out{std::forward<S>(out)} {};
-
-  ~ScopedReset() {
-    if (valid()) {
-      release();
-    }
-  }
-
-  void release() noexcept {
-    m_valid = false;
-    m_out << termseq::reset;
-  }
-
-  bool valid() const noexcept { return m_valid; }
-
- private:
-  bool m_valid{false};
-  Stream m_out;
+  ScopedReset(S&& out)
+      : ScopedOutputParent{std::forward<S>(out), termseq::reset} {}
 };
 
 template <typename T>
